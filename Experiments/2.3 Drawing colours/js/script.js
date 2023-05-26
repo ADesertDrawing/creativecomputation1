9ce64c0474ac27d56deb1772acfc58833cc18b1c
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-05-26 16:12:55 +0100

    Added comments

--- Experiments/2.3 Drawing colours/js/script.js
+++ Experiments/2.3 Drawing colours/js/script.js
@@ -1,9 +1,9 @@
 /**
-Title of Project
-Author Name
+Drawing experiments
+by A Desert Drawing
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Experimenting with p5's drawing and color functions.
+Currently greyscale pyramid scene (scheme).
 */
 
 "use strict";
@@ -18,33 +18,35 @@ function preload() {
 
 
 /**
-Description of setup
+Draws some pyramids and a horizon
 */
 function setup() {
     createCanvas(500, 500);
-    
+    //Set background to off-white
     background(250);
 
+    //Draw left Pyramid
     stroke(220);
     fill(200);
     triangle(130, 250, 60, 380, 200, 380);
     fill(100);
     triangle(130, 250, 200, 380, 220, 350);
 
+    //Draw right pyramid
     noStroke(); 	
-
     fill(200);
     triangle(330, 250, 260, 380, 400, 380);
     fill(100);
     triangle(330, 250, 400, 380, 420, 350);
 
 
-
+    //Draw middle pyramid
     fill(200);
     triangle(230, 300, 160, 430, 300, 430);
     fill(100);
     triangle(230, 300, 300, 430, 320, 400);
 
+    //Draw horizon
     stroke(220);
     strokeWeight(10);
     line(0,100, 500, 90);
@@ -53,7 +55,7 @@ function setup() {
 
 
 /**
-Description of draw()
+Does nothing
 */
 function draw() {
 
