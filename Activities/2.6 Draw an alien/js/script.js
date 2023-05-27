f98813e05de9f39af849c21a1f2c1bfc44a6d0ca
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-05-27 15:34:04 +0100

    Activity 2: Started a new template project

---
+++ Activities/2.6 Draw an alien/js/script.js
@@ -0,0 +1,32 @@
+/**
+2.6 Draw an alien
+by A Desert Drawing
+
+Drawing an alien to show off my muscles
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+    background(255, 255, 0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
