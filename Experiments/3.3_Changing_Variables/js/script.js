c019a2b2bae0c565ec924feca12fb049da8a2d2a
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-02 15:14:00 +0100

    Made a James Bond gun barrel thing using X position plus maths

--- Experiments/3.3_Changing_Variables/js/script.js
+++ Experiments/3.3_Changing_Variables/js/script.js
@@ -1,29 +1,30 @@
 /**
 3.2 creating variables
 by A Desert Drawing
-
-
 */
 
 "use strict";
 
 let backgroundShade=0;
-let circleSize=50;
-let circleX=250;
+let circleSize=150;
+let circleX=0;
 let circleY=250;
-
+let circleSpeed=2;
 
 /**
 Description of setup
 */
 function setup() {
-    createCanvas(500,500);   
+    createCanvas(500,500);  
 }
+
 
 /**
 Description of draw()
 */
 function draw() {
+
     background(backgroundShade);
+    circleX = circleX+circleSpeed;
     ellipse(circleX, circleY, circleSize);
 }
