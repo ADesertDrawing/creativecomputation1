363ff3931a6e06cad2ba22e738492c8e4c775a85
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-02 15:57:30 +0100

    Started 3.4 Javascript objects

---
+++ Experiments/3.4_Introducing_Javascript_Objects/js/script.js
@@ -0,0 +1,31 @@
+/**
+3.4 Introducing Javascript Objects
+by A Desert Drawing
+*/
+
+"use strict";
+
+let backgroundShade=0;
+let circleSize=150;
+let circleX=0;
+let circleY=250;
+let circleSpeed=2;
+let circleAcceleration = 0.25;
+/**
+Description of setup
+*/
+function setup() {
+    createCanvas(500,500);  
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+    background(backgroundShade);
+    circleX += circleSpeed;
+    circleSpeed = circleSpeed + circleAcceleration;
+    ellipse(circleX, circleY, circleSize);
+}
