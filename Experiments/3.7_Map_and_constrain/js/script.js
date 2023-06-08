87775b13b32375e926d0d6254648fd49c284f080
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-08 12:24:23 +0100

    Mapped the mouse position to brightness and constrained position on the right

--- Experiments/3.7_Map_and_constrain/js/script.js
+++ Experiments/3.7_Map_and_constrain/js/script.js
@@ -13,7 +13,7 @@ Description of preload
 */
 let backgroundShade = 0;
 let circle = {
-  x: 250,
+  x: 0,
   y: 250,
   size: 100,
   speed: 1,
@@ -27,6 +27,9 @@ function setup() {
 function draw() {
   background(backgroundShade);
   circle.x = circle.x + circle.speed;
+  circle.x = constrain(circle.x,0,width);
+  circle.size = map(mouseY,0,height,500,50);
+  circle.fill = map(circle.x,0,width, 0, 255);
   fill(circle.fill); 
   ellipse(circle.x, circle.y, circle.size);
 }
