e489ede776a9131306b6ebd91731cd7006eda14d
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-08 14:00:15 +0100

    Activity 3 - started the moving pictures activity

---
+++ Activities/3.8_Moving pictures_activity_03/js/script.js
@@ -0,0 +1,32 @@
+/**
+Title of Project
+by A Desert Drawing
+
+Description
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+    background(255, 255, 0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
