5b068afe3934f94969efdb6c0676e6d3c556fc68
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-09 17:12:32 +0100

    Started on Booleans

---
+++ Experiments/4.3_Booleans/js/script.js
@@ -0,0 +1,23 @@
+/**
+4.3 Booleans
+by A Desert Drawing
+
+
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+    background(255, 255, 0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
