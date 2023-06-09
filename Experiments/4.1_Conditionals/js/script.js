1761f4e8ed4a687ee1e56806d75e2062bed88381
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-09 14:30:14 +0100

    Made a bouncing ball that changes colour based on mouse position!

--- Experiments/4.1_Conditionals/js/script.js
+++ Experiments/4.1_Conditionals/js/script.js
@@ -3,22 +3,50 @@
 by A Desert Drawing
 
 Investigating conditionals
+A ball that bounces left and right and changes colour based on mouseY position
 */
 
 "use strict";
-
+let backgroundShade = 0;
+let leftCanvas = 0;
+let circle = {
+x: 0,
+y: 250,
+size: 100,
+speed: 5
+}
 
 /**
 Description of setup
 */
 function setup() {
-    background(255, 255, 0);
+    createCanvas(500,500) 
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+background(backgroundShade);
+
+circle.x = circle.x + circle.speed;
+
+ellipse(circle.x,circle.y,circle.size);
+
+if (circle.x > width) {
+    circle.speed = -circle.speed;
+}
+
+if (circle.x < leftCanvas) {
+    circle.speed = -circle.speed;
+}
+
+if (mouseY < height/2) {
+    fill(255,0,0);
+}
+
+if (mouseY > height/2) {
+    fill(0,0,255);
+}
 
 }
