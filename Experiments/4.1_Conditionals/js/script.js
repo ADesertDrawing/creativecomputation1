3e1e4e05e208ce3e0beb48de15239d5f5e6c803e
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-09 13:13:51 +0100

    Started experiments on conditionals

---
+++ Experiments/4.1_Conditionals/js/script.js
@@ -0,0 +1,24 @@
+/**
+4.1 Conditionals
+by A Desert Drawing
+
+Investigating conditionals
+*/
+
+"use strict";
+
+
+/**
+Description of setup
+*/
+function setup() {
+    background(255, 255, 0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
