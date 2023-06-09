bf4f291ca5e1647bb8fddcbdbd05c58a23fd0eef
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-09 16:15:51 +0100

    Used else, else if, and, or, & not

--- Experiments/4.2_ More_conditionals/js/script.js
+++ Experiments/4.2_ More_conditionals/js/script.js
@@ -6,17 +6,29 @@ by A Desert Drawing
 
 "use strict";
 
-/**
-Description of setup
-*/
-function setup() {
-    background(255, 255, 0);
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 1
 }
 
+function setup() {
+  createCanvas(500,500);
+}
 
-/**
-Description of draw()
-*/
 function draw() {
+  background(backgroundShade);
 
+  circle.x = circle.x + circle.speed;
+
+    fill(255,255,255);
+    if (!(circle.x < width/3)) {
+            fill(255,0,0);
+        }
+    
+
+
+  ellipse(circle.x,circle.y,circle.size);
 }
