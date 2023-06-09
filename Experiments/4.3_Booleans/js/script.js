65243525c785dfdb6f03a319926a09cf9dc99de9
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-09 19:21:19 +0100

    Made a circle that appears on first mouse click

--- Experiments/4.3_Booleans/js/script.js
+++ Experiments/4.3_Booleans/js/script.js
@@ -7,11 +7,14 @@ by A Desert Drawing
 
 "use strict";
 
+let displayCircle = false;
+
 /**
 Description of setup
 */
 function setup() {
-    background(255, 255, 0);
+    
+    createCanvas(500,500);
 }
 
 
@@ -19,5 +22,14 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(0);
 
+    if (mouseIsPressed) {
+
+        displayCircle = true;
+    }
+
+  if (displayCircle) {
+    ellipse(250,250,100,100);
+  }
 }
