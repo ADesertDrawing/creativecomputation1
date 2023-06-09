729a1563de6616342a6ba77668ed3c9ef5252fd0
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-09 14:54:07 +0100

    Started on More Conditionals if you can believe such a thing

---
+++ Experiments/4.2_ More_conditionals/js/script.js
@@ -0,0 +1,22 @@
+/**
+4.2 More conditionals:
+"Does exactly waht it says on the 'if' statement"
+by A Desert Drawing
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+    background(255, 255, 0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
