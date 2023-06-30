70421c2024050c378ac7343d2d63330105dda525
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-30 11:41:39 +0100

    Used a while loop to drawn caterpillar sections
    Added a while loop, saying that while the segments drawn is less than the number of segments we want (5), we keep drawing them, at 40 pixel intervals, also adding 1 to each one, to keep count

--- Experiments/4.4_Loops/js/script.js
+++ Experiments/4.4_Loops/js/script.js
@@ -7,13 +7,18 @@ Description
 
 "use strict";
 
-
+let caterpillar = {
+    x:100,
+    y:250,
+    segmentSize:50,
+}
 
 /**
 Description of setup
 */
 function setup() {
-    background(255, 255, 0);
+    createCanvas(500,500);
+    
 }
 
 
@@ -21,5 +26,19 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(0);
+    noStroke();
+    fill(100,200,100);
+
+    let x = caterpillar.x;
+    let numSegments = 5;
+    let segmentsDrawn = 0;
+
+    while (segmentsDrawn < numSegments) {
+        ellipse(x,caterpillar.y,caterpillar.segmentSize);
+        x = x+40;
+        segmentsDrawn = segmentsDrawn + 1;
+    } 
+
 
 }
