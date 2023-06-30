8ec90f480f1dad423e19f988aad1adcb7876730e
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-30 15:43:51 +0100

    Started mouse loops experiment

---
+++ Experiments/4.6_Mouse_input/js/script.js
@@ -0,0 +1,22 @@
+/**
+4.6 Mouse input
+by A Desert Drawing
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+    createCanvas(500,500);
+    background(0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
