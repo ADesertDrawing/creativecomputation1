a69dad85136f51b22634906edceda83795b5d799
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-30 14:27:48 +0100

    4.5 started Loops beyond counting

---
+++ Experiments/4.5_Loops_beyond_counting/js/script.js
@@ -0,0 +1,23 @@
+/**
+4.5 Loops beyond counting
+by A Desert Drawing
+
+Description
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+    background(255, 255, 0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
