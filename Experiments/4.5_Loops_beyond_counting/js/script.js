f61d980287719b67706e8f91c747bed954d1ddbe
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-06-30 15:19:13 +0100

    4.5 Why an if statement doesn't work with the danger zone example

--- Experiments/4.5_Loops_beyond_counting/js/script.js
+++ Experiments/4.5_Loops_beyond_counting/js/script.js
@@ -11,16 +11,29 @@ let circle  = {
     x: undefined,
     y: undefined,
     size: 100
-}
+};
+
+let dangerZone = {
+    x: 250,
+    y:250,
+    size:150
+};
 
 /**
 Description of setup
 */
 function setup() {
     createCanvas(500,500);
+
     circle.x = random(0,width);
     circle.y = random(0,height);
-    
+
+    let d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);  
+    if (d < circle.size/2 + dangerZone.size/2) {
+        circle.x = random(0,width);
+        circle.y = random(0,height);
+    }
+
 }
 
 
@@ -29,6 +42,10 @@ Description of draw()
 */
 function draw() {
     background(0);
+//Danger zone
+    noFill();
+    stroke(255,0,0);
+    ellipse(dangerZone.x,dangerZone.y,dangerZone.size);
 
     fill(255);
     noStroke();
