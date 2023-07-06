bb528888855c109ee00deec75d19274cafb270af
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-06 14:44:04 +0100

    Added acceleration to make the movement natural like a buffetted balloon

--- Experiments/4.7_Movement/js/script.js
+++ Experiments/4.7_Movement/js/script.js
@@ -13,7 +13,12 @@ let circle = {
     size: 100,
     vx:0,
     vy:0,
-    speed:5
+    speed:5,
+    ax:0,
+    ay:0,
+    acceleration:0.2,
+    maxSpeed:5,
+
 }
 
 /**
@@ -32,20 +37,26 @@ function draw() {
     background(0,0,0);
 
     if (mouseX < circle.x) {
-        circle.vx=-circle.speed;
+        circle.ax=-circle.acceleration;
     }
 
     else {
-        circle.vx = circle.speed;
+        circle.ax = circle.acceleration;
     }
 
     if (mouseY < circle.y) {
-        circle.vy = -circle.speed;
+        circle.ay = -circle.acceleration;
     }
 
     else {
-        circle.vy = circle.speed;
+        circle.ay = circle.acceleration;
     }
+
+    circle.vx = circle.vx + circle.ax;
+    circle.vx = constrain(circle.vx, -circle.maxSpeed,circle.maxSpeed);
+    circle.vy = circle.vy + circle.ay;
+    circle.vy = constrain(circle.vy, -circle.maxSpeed,circle.maxSpeed);
+
     circle.x = circle.x + circle.vx;
     circle.y = circle.y + circle.vy;
 
