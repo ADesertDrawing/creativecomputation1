76c898046c4171461e91add76227d96399d8d646
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-06 14:52:35 +0100

    4.8 Started Intermediate Drawing

---
+++ Experiments/4.8_Intermediate_drawing/js/script.js
@@ -0,0 +1,24 @@
+/**
+4.8 Intermediate drawing
+by A Desert Drawing
+
+Description
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+    createCanvas(500,500);
+    background(0,0,0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
