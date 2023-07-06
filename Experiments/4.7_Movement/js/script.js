2119ce1e89ef0a45aff03a67b5030e4ddf65800f
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-06 14:24:19 +0100

    The circle chases the pointer around aaaaaaaagh

--- Experiments/4.7_Movement/js/script.js
+++ Experiments/4.7_Movement/js/script.js
@@ -7,12 +7,21 @@ Description
 
 "use strict";
 
+let circle = {
+    x:0,
+    y: 250,
+    size: 100,
+    vx:0,
+    vy:0,
+    speed:5
+}
+
 /**
 Description of setup
 */
 function setup() {
     createCanvas(500,500);
-    background(0,0,0);
+    
 }
 
 
@@ -20,5 +29,25 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(0,0,0);
 
+    if (mouseX < circle.x) {
+        circle.vx=-circle.speed;
+    }
+
+    else {
+        circle.vx = circle.speed;
+    }
+
+    if (mouseY < circle.y) {
+        circle.vy = -circle.speed;
+    }
+
+    else {
+        circle.vy = circle.speed;
+    }
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
+    ellipse(circle.x,circle.y,circle.size);
 }
