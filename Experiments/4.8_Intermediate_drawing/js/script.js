129bced853c38d7dc954b9606d6f006179a35580
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-06 16:34:43 +0100

    Learnt push, pop, rotate, translate, scale

--- Experiments/4.8_Intermediate_drawing/js/script.js
+++ Experiments/4.8_Intermediate_drawing/js/script.js
@@ -6,13 +6,14 @@ Description
 */
 
 "use strict";
-
+let rectScale = 0;
+let angle = 0;
 /**
 Description of setup
 */
 function setup() {
     createCanvas(500,500);
-    background(0,0,0);
+  
 }
 
 
@@ -20,5 +21,17 @@ function setup() {
 Description of draw()
 */
 function draw() {
-
+background(100);
+    push();
+    fill(255,0,0);
+    rectMode(CENTER);
+    translate(width/2,height/2);
+    rotate(angle);
+    scale(rectScale);
+    rect(0,0,100,100);
+    pop();
+    
+    angle = angle + 0.01;
+    rectScale = rectScale + 0.01;
 }
+
