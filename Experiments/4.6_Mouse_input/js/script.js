0666003c169c4e6e28e15a42d073357d3534b04e
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-06 11:31:22 +0100

    Background changes when mouse moves

--- Experiments/4.6_Mouse_input/js/script.js
+++ Experiments/4.6_Mouse_input/js/script.js
@@ -5,12 +5,24 @@ by A Desert Drawing
 
 "use strict";
 
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100
+}
+
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0,
+}
+
 /**
 Description of setup
 */
 function setup() {
     createCanvas(500,500);
-    background(0);
+    
 }
 
 
@@ -18,5 +30,12 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(bg.r,bg.g,bg.b);
+    ellipse(circle.x,circle.y,circle.size);
+}
 
+function mouseMoved() {
+    bg.r = random(0,255);
+    bg.g = random(0,255);
+    bg.b = random(0,255);
 }
