0ad62180a2108a326babca4591e4af2764ce02ce
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-13 16:41:02 +0100

    Debugged the heck out of some conditionals

--- Experiments/4.12_Debugging_conditionals/js/script.js
+++ Experiments/4.12_Debugging_conditionals/js/script.js
@@ -3,20 +3,26 @@
 by A Desert Drawing
 */
 
-"use strict";
+let circle = {
+    x:250,
+    y:250,
+    size:100,
+};
 
-/**
-Description of setup
-*/
 function setup() {
     createCanvas(500,500);
-    background(0,0,0);
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+    background(0,0,0);
 
+    if (mouseX >= width/2) {
+        console.log("Mouse is to the right...");
+        mouseIsLeft = false;
+    }
+    else {
+        console.log("Mouse is to the left...");
+        mouseIsLeft = true;
+    }
+    ellipse(circle.x,circle.y,circle.size);
 }
