c56f5715fa51d30b303539cee095b6663b528bde
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-13 13:31:36 +0100

    Finished avoiding covid activity

--- Activities/4.9_Dodging_COVID19_activity/js/script.js
+++ Activities/4.9_Dodging_COVID19_activity/js/script.js
@@ -1,5 +1,5 @@
 /**
-Title of Project
+4.9 Dodging Covid 19 Activity
 by A Desert Drawing
 
 Description
@@ -7,12 +7,39 @@ Description
 
 "use strict";
 
+let covid19 = {
+    x:0,
+    y:250,
+    size:100,
+    vx:0,
+    vy:0,
+    speed:5,
+    fill: {
+        r:255,
+        g:0,
+        b:0,
+    }
+};
+
+let user = {
+    x:250,
+    y:250,
+    size:100,
+    fill: 255
+};
+
+let numStatic = 1000;
+
 /**
 Description of setup
 */
 function setup() {
-    createCanvas(500,500);
-    background(0,0,0);
+    createCanvas(windowWidth,windowHeight);
+    
+    covid19.y = random(0,height);
+    covid19.vx = covid19.speed;
+
+    noCursor();
 }
 
 
@@ -20,5 +47,39 @@ function setup() {
 Description of draw()
 */
 function draw() {
+    background(0);
+//Display static
+for (let i = 0; i < numStatic; i++) {
+    let x = random(0,width);
+    let y = random(0,height);
+    stroke(255);
+    point(x,y);
+    }
 
+//covid19 movement
+    covid19.x = covid19.x + covid19.vx;
+    covid19.y = covid19.y + covid19.vy;
+
+    if (covid19.x > width) {
+        covid19.x = 0;
+        covid19.y = random(0,height);
+    }   
+//User movement
+    user.x = mouseX;
+    user.y = mouseY;
+
+//Check for catching covid19
+
+    let d = dist(user.x,user.y,covid19.x,covid19.y);
+    if (d < covid19.size/2 + user.size/2) {
+        noLoop();
+    }
+
+//Display covid19
+    fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+    ellipse(covid19.x,covid19.y,covid19.size);
+
+//Display user
+    fill(user.fill);
+    ellipse(user.x,user.y,user.size);
 }
