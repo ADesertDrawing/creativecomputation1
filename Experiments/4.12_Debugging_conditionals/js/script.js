9c457ea59aff00d52a6b65f750dd7753732aed0b
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-13 16:02:58 +0100

    You're bugging my conditionals!

---
+++ Experiments/4.12_Debugging_conditionals/js/script.js
@@ -0,0 +1,22 @@
+/**
+4.12 Debugging conditionals
+by A Desert Drawing
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+    createCanvas(500,500);
+    background(0,0,0);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
