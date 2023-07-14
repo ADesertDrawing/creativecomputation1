5cdad23e10e92800997e7e3c7886b087fe7d4faf
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-14 12:51:08 +0100

    Made some pret-ty nice lines actually

--- Experiments/5.2_Functions_with_parameters/js/script.js
+++ Experiments/5.2_Functions_with_parameters/js/script.js
@@ -10,4 +10,19 @@ function setup() {
 
 function draw() {
     background(0,0,0);
+    parallels(100,101,24,2,550,4);
+    parallels(50,52,43,3,100,10);
+    parallels(200,207,19,27,125,4);
+    parallels(312,73,52,5,250,29);
+}
+
+function parallels(x,y,numLines,lineWidth,lineHeight,lineSpacing) {
+
+    for (let i = 0; i < numLines; i++) {
+      noStroke();
+      fill(255); 
+      rectMode(CENTER);
+      rect(x,y,lineWidth,lineHeight);
+      x = x + lineSpacing; 
+    }
 }
