c5320a3d8fe6b857572c10a6a99fd5f4f7829b67
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-14 16:21:13 +0100

    Used a function with a return value

--- Experiments/5.3_Functions_with_return_values/js/script.js
+++ Experiments/5.3_Functions_with_return_values/js/script.js
@@ -6,8 +6,17 @@ by A Desert Drawing
 function setup() {
     createCanvas(500,500);
 
+    let hotCelsius = toCelsius(100);
+    let coldCelsius = toCelsius(10);
+    console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`);
+    console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`);
 }
 
 function draw() {
     background(0,0,0);
+}
+
+function toCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
 }
