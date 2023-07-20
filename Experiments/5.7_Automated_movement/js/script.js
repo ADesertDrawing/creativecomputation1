69ecc1ec5ae20d4a9002518921b902aa4792281f
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-20 17:28:15 +0100

    Made the mouse chase the cursor

--- Experiments/5.7_Automated_movement/js/script.js
+++ Experiments/5.7_Automated_movement/js/script.js
@@ -3,11 +3,47 @@
 by A Desert Drawing
 */
 
-function setup() {
-    createCanvas(500,500);
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2
+}
 
+function setup() {
+  createCanvas(500, 500);
+
+  // Linear movement down and to the right
+  circle.vx = circle.speed;
+  circle.vy = circle.speed;
 }
 
 function draw() {
-    background(0,0,0);
+  background(0);
+
+  let dx = circle.x - mouseX;
+  let dy = circle.y - mouseY;
+
+  let change = random();
+  if (dx < 0) {
+    circle.vx = circle.speed;
+  } 
+  else if (dx > 0) {
+    circle.vx = -circle.speed;
+  }
+
+  if (dy < 0) {
+    circle.vy = circle.speed;
+  }
+  else if (dy > 0) {
+    circle.vy = -circle.speed;
+  }
+  
+
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
 }
