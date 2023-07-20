ee28293016916adb7e5bc1b67c54dd7d164bf6fa
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-20 12:41:57 +0100

    Made text move and get bigger deal with it

--- Experiments/5.4_Text/js/script.js
+++ Experiments/5.4_Text/js/script.js
@@ -1,7 +1,16 @@
 /**
-Title of Project
+5.4 Text
 by A Desert Drawing
 */
+
+let hello = {
+    string: `Hello, world`,
+    x:0,
+    y: 0,
+    vx: 5,
+    vy: 1,
+    size: 64
+};
 
 function setup() {
     createCanvas(500,500);
@@ -9,5 +18,17 @@ function setup() {
 }
 
 function draw() {
-    background(0,0,0);
+hello.x = hello.x + hello.vx;
+hello.y = hello.y + hello.vy;
+
+hello.size = hello.size + 1;
+
+    background(0);
+    textAlign(CENTER,CENTER);
+    stroke(50,200,50);
+    strokeWeight(5);
+    textStyle(BOLD);
+    textSize(hello.size);
+    fill(200,50,200);
+    text(hello.string,hello.x,hello.y);
 }
