97219b09ae594a843958569a32880fa8b167a1d5
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-28 14:21:49 +0100

    Started basic sound

---
+++ Experiments/6.1_Basic_sound/js/script.js
@@ -0,0 +1,13 @@
+/**
+6.1 Basic sound
+by A Desert Drawing
+*/
+
+function setup() {
+    createCanvas(500,500);
+
+}
+
+function draw() {
+    background(0,0,0);
+}
--- Games/Curiosityp5/js/script.js
+++ Games/Curiosityp5/js/script.js
@@ -3,11 +3,55 @@ Curiosity in p5
 by A Desert Drawing
 */
 
-function setup() {
-    createCanvas(500,500);
+// A variable to store the font in
+//let VT323Font;
 
+
+
+// Load the font in preload
+function preload() {
+ // VT323Font = loadFont(`assets/fonts/VT323-Regular.ttf`);
 }
+
+let current = "introduction";
+
+const story = {
+  "introduction": {
+    "main_text": "You are standing at the mouth of a cave.",
+    "links": [
+      "Enter the cave", "cave",
+      "Run away", "ran away"
+    ]
+  },
+  "cave": {
+    "main_text": "You are inside the cave, it is very dark.",
+    "links": []   
+  },
+  "ran away": {
+    "main_text": "You are a terrible coward and will never learn anything about spelunking.",
+    "links": []
+  }
+}
+
+function setup() {
+  createCanvas(200,200);
+}
+
+
 
 function draw() {
-    background(0,0,0);
+  background(255);
+
+  //fill(0,255,0);
+  // Use the font by passing the font variable as an argument to textFont()
+  //text(VT323Font);
+
+  text(0, 100, story[current].main_text);
+  // Display the links
+  for (let i = 0; i < story[current].links.length; i++) {
+     // Display the links... though right now I'm realizing we'd need a way
+     // to remember where the links lead, which is another layer of complexity... needs more though that I have time for this instant, but for now
+     text(100, 100, story[current].links[i]);   
+  }
 }
+
