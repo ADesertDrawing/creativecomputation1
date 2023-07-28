ca3bcae247bfb8cfdd5d14de4ecda406d9be3bdd
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-28 15:07:20 +0100

    Added looping sounds, triggers and checks to see if already playing

--- Experiments/6.1_Basic_sound/js/script.js
+++ Experiments/6.1_Basic_sound/js/script.js
@@ -3,11 +3,31 @@
 by A Desert Drawing
 */
 
+let music;
+
+function preload() {
+    music = loadSound(`assets/sounds/bark.wav`);
+}
+
 function setup() {
     createCanvas(500,500);
 
 }
 
 function draw() {
-    background(0,0,0);
+    background(0);
+}
+
+function mousePressed() {
+    tryMusic();
+    }
+
+function keyPressed() {
+    tryMusic();
+    }
+
+function tryMusic() {
+    if (!music.isPlaying()) {
+    music.loop();
+}
 }
