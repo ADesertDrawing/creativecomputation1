5cfbfb99cd493d48dbc15a9fcebf84a6723b61b5
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-07-28 15:32:37 +0100

    finished use strict

--- Experiments/6.2_use_strict/js/script.js
+++ Experiments/6.2_use_strict/js/script.js
@@ -3,6 +3,14 @@
 by A Desert Drawing
 */
 
+"use strict";
+
+let circle = {
+    x: 0,
+    y: 0,
+    size: 100
+}
+
 function setup() {
     createCanvas(500,500);
 
@@ -10,4 +18,7 @@ function setup() {
 
 function draw() {
     background(0,0,0);
+circle.x = mouseX;
+circley = mouseY;
+    ellipse(circle.x,circle.y,100);
 }
