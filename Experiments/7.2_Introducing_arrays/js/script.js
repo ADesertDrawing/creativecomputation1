025a4173de9530815f931996c13272ad94141769
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-09-29 15:01:48 +0100

    Started 7.2 Introducing arrays with a nice aquarium of pink round fish

---
+++ Experiments/7.2_Introducing_arrays/js/script.js
@@ -0,0 +1,81 @@
+/**
+7.2 Introducing arrays
+by A Desert Drawing
+*/
+
+"use strict";
+
+// Our fish
+let fish1;
+let fish2;
+let fish3;
+let fish4;
+
+function setup() {
+  createCanvas(600, 600);
+
+  // Create four fish, positioned randomly
+  fish1 = createFish(random(0, width), random(0, height));
+  fish2 = createFish(random(0, width), random(0, height));
+  fish3 = createFish(random(0, width), random(0, height));
+  fish4 = createFish(random(0, width), random(0, height));
+}
+
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  };
+  return fish;
+}
+
+// draw()
+// Moves and displays our fish
+function draw() {
+  background(0);
+
+  moveFish(fish1);
+  moveFish(fish2);
+  moveFish(fish3);
+  moveFish(fish4);
+
+  displayFish(fish1);
+  displayFish(fish2);
+  displayFish(fish3);
+  displayFish(fish4);
+}
+
+// moveFish(fish)
+// Chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  // Choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  // Move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // Constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+// displayFish(fish)
+// Displays the provided fish on the canvas
+function displayFish(fish) {
+  push();
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
+}
