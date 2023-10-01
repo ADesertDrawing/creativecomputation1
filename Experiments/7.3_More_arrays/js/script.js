e8fab7fcfd8482f8d50455450ee07ae9e4e58ab9
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-10-01 18:20:52 +0100

    Barking dog singalong!

--- Experiments/7.3_More_arrays/js/script.js
+++ Experiments/7.3_More_arrays/js/script.js
@@ -4,39 +4,24 @@ by A Desert Drawing
 */
 "use strict";
 
-let circle = {
-    x: 0,
-    y: 0,
-    size:100,
-    trail: [],
-    trailSize: 20,
-}
+let barkSFX;
 
-let currentIndex = 0;
+let rates = [1.5,1.75,2.25,2.5,2.75,3];
+
+function preload() {
+    barkSFX = loadSound(`assets/sounds/bark.wav`);
+}
 
 function setup() {
     createCanvas(600,600);
 }
 
 function draw() {
-    background(random(0,255));
-    circle.x = mouseX;
-    circle.y = mouseY;
+    background(0);
+}
 
-    for (let i = 0; i < circle.trail.length; i++) {
-        let position = circle.trail[i];
-        ellipse(position.x,position.y,circle.size);
-    }
-
-    ellipse(circle.x,circle.y, circle.size);
-
-    let newTrailPosition = {
-        x: circle.x,
-        y: circle.y
-    };
-    circle.trail.push(newTrailPosition);
-    if (circle.trail.length > circle.trailSize) {
-        circle.trail.shift();
-    }
-
+function mousePressed() {
+    let randomRate = random(rates);
+    barkSFX.rate(randomRate);
+    barkSFX.play();
 }
