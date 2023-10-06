891f7d0387bc517a486348822bddc18e859a6413
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-10-06 11:39:45 +0100

    Started 8.2 Constructors with parameters

---
+++ Experiments/8.2_Constructors_with_parameters/js/script.js
@@ -0,0 +1,13 @@
+/**
+8.2 Constructors with parameters
+by A Desert Drawing
+*/
+"use strict";
+function setup() {
+    createCanvas(500, 500);
+
+}
+
+function draw() {
+    background(0, 0, 0);
+}
