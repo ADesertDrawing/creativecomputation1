b57c216116aea8843cbef75ee800ee27a55765e6
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-10-06 17:27:51 +0100

    Adding shrinkage

---
+++ Experiments/8.4_Interacting_objects/js/script.js
@@ -0,0 +1,56 @@
+/**
+8.4 Interacting objects
+by A Desert Drawing
+*/
+"use strict";
+
+// Our garden
+let garden = {
+    // An array to store the individual flowers
+    flowers: [],
+    // How many flowers in the garden
+    numFlowers: 20,
+    // The color of the grass (background)
+    grassColor: {
+        r: 120,
+        g: 180,
+        b: 120
+    }
+};
+
+// setup() creates the canvas and the flowers in the garden
+function setup() {
+    createCanvas(600, 600);
+
+    // Create our flowers by counting up to the number of the flowers
+    for (let i = 0; i < garden.numFlowers; i++) {
+        // Create variables for our arguments for clarity
+        let x = random(0, width);
+        let y = random(0, height);
+        let size = random(50, 80);
+        let stemLength = random(50, 100);
+        let petalColor = {
+            r: random(100, 255),
+            g: random(100, 255),
+            b: random(100, 255)
+        }
+        // Create a new flower using the arguments
+        let flower = new Flower(x, y, size, stemLength, petalColor);
+        // Add the flower to the array of flowers
+        garden.flowers.push(flower);
+    }
+}
+
+
+// draw()
+// Displays our flowers
+function draw() {
+    // Display the grass
+    background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+
+    // Loop through all the flowers in the array and display them
+    for (let i = 0; i < garden.flowers.length; i++) {
+        let flower = garden.flowers[i];
+        flower.display();
+    }
+}
