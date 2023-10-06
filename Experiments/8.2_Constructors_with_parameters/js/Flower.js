896a81f864487d3abba45299b7280feb1a48a224
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-10-06 12:16:12 +0100

    Added variation to flowers by adding parameters to the constructor

---
+++ Experiments/8.2_Constructors_with_parameters/js/Flower.js
@@ -0,0 +1,42 @@
+class Flower {
+
+    // The constructor() sets up a flower's properties
+    constructor(x, y, size, stemLength, petalColor) {
+        // Position and size information
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.stemLength = stemLength;
+        this.stemThickness = 10;
+        this.petalThickness = 10;
+        // Color information
+        this.stemColor = {
+            r: 50,
+            g: 150,
+            b: 50
+        };
+        this.petalColor = petalColor;
+        this.centreColor = {
+            r: 50,
+            g: 0,
+            b: 0
+        };
+    }
+
+    // display()
+    // Displays the flower on the canvas
+    display() {
+        push();
+        // Set the stroke weight for the petals and the stem
+        strokeWeight(this.stemThickness);
+        // Draw a line for the stem
+        stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
+        line(this.x, this.y, this.x, this.y + this.stemLength);
+        // Draw a circle with a heavy outline for the flower
+        strokeWeight(this.petalThickness);
+        fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
+        stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
+        ellipse(this.x, this.y, this.size);
+        pop();
+    }
+}
--- Experiments/8.2_Constructors_with_parameters/js/script.js
+++ Experiments/8.2_Constructors_with_parameters/js/script.js
@@ -3,11 +3,54 @@
 by A Desert Drawing
 */
 "use strict";
+
+// Our garden
+let garden = {
+    // An array to store the individual flowers
+    flowers: [],
+    // How many flowers in the garden
+    numFlowers: 20,
+    // The color of the grass (background)
+    grassColor: {
+        r: 120,
+        g: 180,
+        b: 120
+    }
+};
+
+// setup() creates the canvas and the flowers in the garden
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(600, 600);
+
+    // Create our flowers by counting up to the number of the flowers
+    for (let i = 0; i < garden.numFlowers; i++) {
+        let x = random(0, width);
+        let y = random(0, height);
+        let size = random(50, 80);
+        let stemLength = random(50, 100);
+        let petalColor = {
+            r: random(100, 255),
+            g: random(100, 255),
+            b: random(100, 255),
+        };
+        // Create a new flower
+        let flower = new Flower(x, y, size, stemLength, petalColor);
+        // Add the flower to the array of flowers
+        garden.flowers.push(flower);
+    }
 
 }
 
+// draw()
+// Displays our flowers
 function draw() {
-    background(0, 0, 0);
+    // Display the grass
+    background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
+
+    // Loop through all the flowers in the array and display them
+    for (let i = 0; i < garden.flowers.length; i++) {
+        let flower = garden.flowers[i];
+        flower.display();
+    }
 }
+
