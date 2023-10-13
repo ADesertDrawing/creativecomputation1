57cb7755905a22a0efff8ccfc5d3d82a841bb3da
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-10-13 14:25:55 +0100

    Started 8.5 Object-Oriented Programming activity

---
+++ Activities/8.5_Object_oriented_programming activity/js/script.js
@@ -0,0 +1,13 @@
+/**
+8.5 Object-Oriented Programming activity
+by A Desert Drawing
+*/
+"use strict";
+function setup() {
+    createCanvas(500, 500);
+
+}
+
+function draw() {
+    background(0, 0, 0);
+}
