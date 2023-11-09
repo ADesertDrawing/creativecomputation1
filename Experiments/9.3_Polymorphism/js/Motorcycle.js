40cce980c8953d1907e8f0f0f80f9aa3a02b92e8
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-11-09 15:46:09 +0000

    Finished polymorphism, working with groups of objects that share methods and properties from a superclass.

---
+++ Experiments/9.3_Polymorphism/js/Motorcycle.js
@@ -0,0 +1,18 @@
+class Motorcycle extends Vehicle {
+    constructor(x, y) {
+        super(x, y);
+        this.width = 30;
+        this.height = 10;
+        this.vx = 10;
+    }
+
+    display() {
+        push();
+        rectMode(CENTER);
+        noStroke();
+        fill(255, 255, 0);
+        rect(this.x, this.y, this.width, this.height);
+        pop();
+    }
+}
+
