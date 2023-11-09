052df1b8a146e32508add508fb6b665b8342eb94
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-11-09 15:16:15 +0000

    The road is full of drunk drivers.

--- Experiments/9.2_Overriding_methods/js/Car.js
+++ Experiments/9.2_Overriding_methods/js/Car.js
@@ -4,6 +4,30 @@ class Car extends Vehicle {
         this.width = 50;
         this.height = 20;
         this.vx = 5;
+        this.drunkenness = 0.2;
+    }
+
+    move() {
+        this.veer();
+
+        super.move();
+    }
+
+    veer() {
+        let r = random();
+        if (r < this.drunkenness) {
+            this.vy = random(-5, 5);
+        }
+    }
+
+    wrap() {
+        super.wrap();
+        if (this.y > height) {
+            this.y -= height;
+        }
+        else if (this.y < 0) {
+            this.y += height;
+        }
     }
 
     display() {
