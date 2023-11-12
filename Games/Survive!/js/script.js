b09ee4fa092441508e4a41c3a43ee8410cb99800
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-11-12 17:37:09 +0000

    Started Survive! and added basic timer

--- Games/Survive!/js/script.js
+++ Games/Survive!/js/script.js
@@ -1,13 +1,21 @@
 /**
-Title of Project
+Survive!
 by A Desert Drawing
 */
+
+
 "use strict";
+let timer = 0;
+
 function setup() {
     createCanvas(500, 500);
 
 }
 
 function draw() {
-    background(0, 0, 0);
+    background(220);
+    textSize(20);
+
+    text(round(timer, 2), 200, 200);
+    timer += 1 / 60;
 }
