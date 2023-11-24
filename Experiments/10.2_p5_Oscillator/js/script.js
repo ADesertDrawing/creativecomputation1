e019de5f52f0970ffd061bdd92a4f3458580636c
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-11-24 15:25:21 +0000

    Setting up

--- Experiments/10.2_p5_Oscillator/js/script.js
+++ Experiments/10.2_p5_Oscillator/js/script.js
@@ -2,12 +2,18 @@
 10.2_p5_Oscillator
 by A Desert Drawing
 */
+
 "use strict";
 function setup() {
     createCanvas(500, 500);
-
+    userStartAudio();
 }
 
 function draw() {
     background(0, 0, 0);
 }
+
+function mousePressed() {
+
+}
+
