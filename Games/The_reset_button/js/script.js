069b88f43b3bf43b1ab5efb7d536ea4e82b7ada7
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-11-30 17:04:58 +0000

    Commented. ButtonDown doesn't stay visible for long.

--- Games/The_reset_button/js/script.js
+++ Games/The_reset_button/js/script.js
@@ -6,14 +6,17 @@ by A Desert Drawing
 
 let state = `buttonUp`; //can be buttonUp or buttonDown
 
+//A small delay after showing the buttonDown to reverting to buttonUp
+let smallDelay;
+let delayMax = 60;
+
+// Setting up the images/sound for buttonUp/buttonDown/buttonSound
 let resetButtonDown;
 let resetButtonUp;
+
 let buttonSound;
 
-
-
-
-
+// Preloading the images and sounds
 function preload() {
     resetButtonDown = loadImage(`assets/images/resetButtonDown.png`);
     resetButtonUp = loadImage(`assets/images/resetButtonUp.png`);
@@ -21,10 +24,11 @@ function preload() {
 }
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    background(0);
+    // setTimeout(buttonDown, 3000);
 }
-
+//Setting up the states (only buttonUp and buttonDown)
 function draw() {
-
     if (state === `buttonUp`) {
         buttonUp();
     }
@@ -33,21 +37,30 @@ function draw() {
     }
 }
 
-function buttonUp() {
-    image(resetButtonUp, 0, 0, 600, 300);
-}
-
-function buttonDown() {
-    image(resetButtonDown, 0, 0, 600, 300);
-    buttonSound.play();
-}
-
+//Mouseclick changes state to buttonDown
 function mousePressed() {
     if (state === `buttonUp`) {
-        image(resetButtonUp, 0, 0, 600, 300);
+        state = `buttonDown`;
     }
-    else if (state === `buttonDown`) {
-        image(resetButtonDown, 0, 0, 600, 300);
+}
+//Show buttonUp image
+function buttonUp() {
+    background(0);
+    image(resetButtonUp, width / 2, height / 2, 300, 150);
+}
+
+//Show buttonDown image (offset to test if it's appearing)
+function buttonDown() {
+    image(resetButtonDown, width / 2 + 50, height / 2 + 50, 300, 150);
+
+    if (!buttonSound.isPlaying()) {
         buttonSound.play();
     }
+
+    while (smallDelay <= delayMax) {
+        smallDelay = smallDelay + 1;
+    }
+    //Set the state back to buttonUp to revert to original state
+    state = `buttonUp`;
+
 }
