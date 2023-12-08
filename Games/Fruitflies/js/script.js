95e034c91482264036226b41534698d450e7b2ed
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-12-08 17:39:04 +0000

    Added Person class and movement images

--- Games/Fruitflies/js/script.js
+++ Games/Fruitflies/js/script.js
@@ -3,6 +3,28 @@ Fruitflies
 by A Desert Drawing
 */
 "use strict";
+
+//Setting up all the player movement images
+let leftLeft;
+let leftBack;
+let middleBack;
+let rightBack;
+let rightRight;
+let rightFront;
+let middleFront;
+let leftFront;
+
+function preload() {
+    //Preloading the player movement images
+    leftLeft = loadImage('/leftLeft.png');
+    leftBack = loadImage('/leftBack.png');
+    middleBack = loadImage('/middleBack.png');
+    rightBack = loadImage('/rightBack.png');
+    rightRight = loadImage('/rightRight.png');
+    rightFront = loadImage('/rightFront.png');
+    middleFront = loadImage('/middleFront.png');
+    leftFront = loadImage('/leftFront.png');
+}
 function setup() {
     createCanvas(600, 600);
 
