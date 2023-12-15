2de0d7b2b2ea9622d4f9f149426601094eae9520
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-12-15 15:45:04 +0000

    Oscillator with noise!

--- Experiments/10.2_p5_Oscillator/js/script.js
+++ Experiments/10.2_p5_Oscillator/js/script.js
@@ -6,19 +6,25 @@ by A Desert Drawing
 "use strict";
 
 let oscillator;
+let t = 0;
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(600, 600);
     userStartAudio();
 
     oscillator = new p5.Oscillator(440, `sine`);
+    oscillator.amp(0.2);
 }
 
 function draw() {
     background(0, 0, 0);
+    let noiseValue = noise(t);
 
-    let newFreq = map(mouseY, height, 0, 0, 880);
+    //Put that into our variable called `newFreq`
+    let newFreq = map(noiseValue, 0, 1, 20, 2000);
+    //Set the newFreq as the frequency of our oscillator
     oscillator.freq(newFreq);
+    t = t + 0.05;
 }
 
 function mousePressed() {
