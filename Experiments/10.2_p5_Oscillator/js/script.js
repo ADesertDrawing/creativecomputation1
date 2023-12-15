7a1bf6c62fba8a96ef9d10effcc5fc88e1852827
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2023-12-15 13:52:51 +0000

    A nice oscillator based on mouseY position

--- Experiments/10.2_p5_Oscillator/js/script.js
+++ Experiments/10.2_p5_Oscillator/js/script.js
@@ -4,16 +4,27 @@ by A Desert Drawing
 */
 
 "use strict";
+
+let oscillator;
+
 function setup() {
     createCanvas(500, 500);
     userStartAudio();
+
+    oscillator = new p5.Oscillator(440, `sine`);
 }
 
 function draw() {
     background(0, 0, 0);
+
+    let newFreq = map(mouseY, height, 0, 0, 880);
+    oscillator.freq(newFreq);
 }
 
 function mousePressed() {
-
+    oscillator.start();
 }
 
+function mouseReleased() {
+    oscillator.stop();
+}
