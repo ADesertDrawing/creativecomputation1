0f7bfe7edd7ad6da25a2ea4a7800897471f66d60
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-01-01 18:23:31 +0000

    Adding functions for mouse pressings

--- Games/ProblemSolution/js/script.js
+++ Games/ProblemSolution/js/script.js
@@ -20,6 +20,29 @@ function setup() {
 
 function draw() {
     background(255);
+
+
     image(scalesBottom, 0, 0);
     image(scalesTop, 0, 0);
+}
+
+function mousePressed() {
+    if (mouseX <= width / 2) {
+        leftDown;
+    }
+    else if (mouseX > width / 2) {
+        rightDown;
+    }
+}
+
+function leftDown() {
+
+    translate(width / 2, height / 2);
+    rotate(PI / 180 * 45);
+    imageMode(CENTER);
+    image(scalesTop, 0, 0, 150, 150);
+}
+
+function rightDown() {
+
 }
