23012c0302f52e64a686c223a60b3be29d440941
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-01-01 18:02:09 +0000

    Drawn the top and bottom layers

--- Games/ProblemSolution/js/script.js
+++ Games/ProblemSolution/js/script.js
@@ -3,11 +3,23 @@ Problem / Solution
 by A Desert Drawing
 */
 "use strict";
+
+let scalesBottom;
+let scalesTop;
+
+function preload() {
+    //Preloading the base bg layer and the top scales layer
+    scalesBottom = loadImage('assets/images/prob_sol_bottom_1000x700.png');
+    scalesTop = loadImage('assets/images/prob_sol_top_1000x700.png');
+}
+
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(1000, 700);
 
 }
 
 function draw() {
-    background(0, 0, 0);
+    background(255);
+    image(scalesBottom, 0, 0);
+    image(scalesTop, 0, 0);
 }
