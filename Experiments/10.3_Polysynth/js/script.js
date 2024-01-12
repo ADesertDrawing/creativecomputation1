4676bf716003a1b9ef979eec3c83965b015021c4
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-01-12 14:32:37 +0000

    An F-minor scale at the drop of a minor hat

--- Experiments/10.3_Polysynth/js/script.js
+++ Experiments/10.3_Polysynth/js/script.js
@@ -6,6 +6,8 @@ by A Desert Drawing
 "use strict";
 
 let synth;
+let notes = [`F2`, `G2`, `Ab3`, `Bb3`, `C3`, `Db3`, `Eb3`, `F3`]; //F-minor 
+let currentNote = 0;
 
 function setup() {
     createCanvas(600, 600);
@@ -17,6 +19,16 @@ function draw() {
     background(0);
 }
 
-function mousePressed() {
-    synth.play(`C4`, 1, 0, 1);
+function keyPressed() {
+    setInterval(playRandomNote, 150);
+}
+
+function playRandomNote() {
+    let note = notes[currentNote];
+    synth.play(note, 1, 0, 0.1);
+
+    currentNote = currentNote + 1;
+    if (currentNote === notes.length) {
+        currentNote = 0;
+    }
 }
