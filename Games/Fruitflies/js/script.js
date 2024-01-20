c62f190e38e61ccde744beb2c091fefda7d026c7
ADesertDrawing <132758861+ADesertDrawing@users.noreply.github.com>
2024-01-20 16:56:46 +0000

    We have movement. Need to stop the sprite images overlapping.

--- Games/Fruitflies/js/script.js
+++ Games/Fruitflies/js/script.js
@@ -14,22 +14,104 @@ let rightFront;
 let middleFront;
 let leftFront;
 
+let person;
+//This stores the laaast key press so we can delete it and go to 
+//the next one, so we can have two pressed at once (diagonals)
+let pressedKeys = {};
+
 function preload() {
     //Preloading the player movement images
-    leftLeft = loadImage('/leftLeft.png');
-    leftBack = loadImage('/leftBack.png');
-    middleBack = loadImage('/middleBack.png');
-    rightBack = loadImage('/rightBack.png');
-    rightRight = loadImage('/rightRight.png');
-    rightFront = loadImage('/rightFront.png');
-    middleFront = loadImage('/middleFront.png');
-    leftFront = loadImage('/leftFront.png');
+    leftLeft = loadImage('assets/images/leftLeft.png');
+    leftBack = loadImage('assets/images//leftBack.png');
+    middleBack = loadImage('assets/images//middleBack.png');
+    rightBack = loadImage('assets/images//rightBack.png');
+    rightRight = loadImage('assets/images//rightRight.png');
+    rightFront = loadImage('assets/images//rightFront.png');
+    middleFront = loadImage('assets/images//middleFront.png');
+    leftFront = loadImage('assets/images//leftFront.png');
+
+
 }
 function setup() {
     createCanvas(600, 600);
-
+    person = new Person(width / 2, height / 2);
 }
+
+
 
 function draw() {
-    background(0, 0, 0);
+    background(200);
+    person.update();
+    person.display();
+
 }
+//Our pressedKeys will only hold whatever keys we're holding
+function keyPressed() {
+    pressedKeys[key] = true;
+}
+//and then remove them
+function keyReleased() {
+    delete pressedKeys[key];
+
+}
+
+class Person {
+    constructor(x, y,) {
+        this.x = x;
+        this.y = y;
+        this.size = 50;
+        this.speed = 3;
+
+
+    }
+
+    update() {
+        let mvmt = createVector(0.0);
+
+        if (pressedKeys.a) {
+            mvmt.x -= 1;
+        }
+        if (pressedKeys.d) {
+            mvmt.x += 1;
+        }
+        if (pressedKeys.w) {
+            mvmt.y -= 1;
+        }
+        if (pressedKeys.s) {
+            mvmt.y += 1;
+        }
+
+        mvmt.setMag(this.speed);
+
+        this.x += mvmt.x;
+        this.y += mvmt.y;
+    }
+
+    move() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    display() {
+        if (pressedKeys.a) {
+            image(leftLeft, this.x, this.y, 50, 100);
+        }
+        if (pressedKeys.d) {
+            image(rightRight, this.x, this.y, 50, 100);
+        }
+        if (pressedKeys.w) {
+            image(middleBack, this.x, this.y, 50, 100);
+
+        }
+        if (pressedKeys.s) {
+            image(middleFront, this.x, this.y, 50, 100);
+        }
+
+        if (pressedKeys.s & pressedKeys.a) {
+            image(leftFront, this.x, this.y, 50, 100);
+
+        }
+    }
+
+}
+
